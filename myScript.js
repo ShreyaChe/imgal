a5d607bb14138c24e0010290abae2373d4860d01
ShreyaChe <shreya.ariesk@gmail.com>
2019-11-30 18:10:17 +0530

    Initial Commit

---
+++ myScript.js
@@ -0,0 +1,42 @@
+var x =  "Leveraging Social Proof for Growth";
+var y = "Duolingo removes language barriers by connecting people that need websites translated with students that are learning a language.Through trial and error the team at Duolingo has astered the art of data driven product development through AB testing and focus on the right metrics to optimize.driven product development through AB testing and a focus on the right metrics to optimize.";
+var z = " &#x1F64E;Anna   &#x1F552; 2 days ago "
+document.getElementById("title").innerHTML = x ;
+document.getElementById("para").innerHTML = y ;
+document.getElementById("post").innerHTML =" &#x1F64E;Anna   &#x1F552; Today  ";
+
+document.getElementById("ftitle").innerHTML = x ;
+document.getElementById("fpara").innerHTML = y ;
+document.getElementById("fpost").innerHTML =z;
+
+document.getElementById("ctitle").innerHTML = x ;
+document.getElementById("cpara").innerHTML = y ;
+document.getElementById("cpost").innerHTML =z;
+
+document.getElementById("cutitle").innerHTML = x ;
+document.getElementById("cupara").innerHTML = y ;
+document.getElementById("cupost").innerHTML =z;
+
+document.getElementById("btitle").innerHTML = x ;
+document.getElementById("bpara").innerHTML = y ;
+document.getElementById("bpost").innerHTML =z;
+
+
+document.getElementById("dtitle").innerHTML = x ;
+document.getElementById("dpara").innerHTML = y ;
+document.getElementById("dpost").innerHTML =z;
+
+
+document.getElementById("fatitle").innerHTML = x ;
+document.getElementById("fapara").innerHTML = y ;
+document.getElementById("fapost").innerHTML =z;
+
+
+
+document.getElementById("ptitle").innerHTML = x ;
+document.getElementById("ppara").innerHTML = y ;
+document.getElementById("ppost").innerHTML =z;
+
+document.getElementById("dttitle").innerHTML = x ;
+document.getElementById("dtpara").innerHTML = y ;
+document.getElementById("dtpost").innerHTML =z;
